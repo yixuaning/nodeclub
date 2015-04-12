cb4e1afc7589d1002e4092afa72d2f03096ab97c
yixuaning <yixuaning@sina.com>
2015-04-12 17:28:15 +0800

    设置logo为37iot.svg

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var config = {
   site_headers: [
     '<meta name="author" content="yasin.guo@aliyun.com" />'
   ],
-  site_logo: '/public/images/cnodejs_light.svg', // default is `name`
+  site_logo: '/public/images/37iot.svg', // default is `name`
   site_icon: '/public/images/cnode_icon_32.png', // 默认没有 favicon, 这里填写网址
   // 右上角的导航区
   site_navs: [
